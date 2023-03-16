b45a4349cabd165edc4b3d623f8b2d6a2c6199fe
Shadow <96359879+ShadowRG2023@users.noreply.github.com>
2023-03-16 13:58:35 -0400

    Add files via upload

--- GriklorSleet.lua
+++ GriklorSleet.lua
@@ -2,7 +2,7 @@ GriklorSleet
 
 Import Code:
 
-cmV0dXJuIHsKIGxvYWQgPSB7CiAgYWx3YXlzID0gZmFsc2UsCiAgY2xhc3MgPSAiIiwKICB6b25lID0gIiIsCiB9LAogY29kZSA9ICJiRzlqWVd3Z2JYRWdQU0J5WlhGMWFYSmxLQ2R0Y1NjcENteHZZMkZzSUc5eklEMGdjbVZ4ZFdseVpTZ25iM01uS1Fwc2IyTmhiQ0J0ZVY5amJHRnpjeUE5SUcxeExsUk1UeTVOWlM1RGJHRnpjeTVUYUc5eWRFNWhiV1VvS1FvS2JHOWpZV3dnWm5WdVkzUnBiMjRnWlhabGJuUmZhR0Z1Wkd4bGNpZ3BDaUFnSUNCc2IyTmhiQ0J6ZEdGeWRGUnBiV1VnUFNCdmN5NTBhVzFsS0NrS0lDQWdJR2xtSUcxNVgyTnNZWE56SUQwOUlDZENSVkluSUdGdVpDQnRjUzVVVEU4dVRXVXVRV04wYVhabFJHbHpZeTVPWVcxbEtDa2dQVDBnYlhFdVZFeFBMbE53Wld4c0tDZEdjbVZ1ZW1sbFpDQlNaWE52YkhabElFUnBjMk5wY0d4cGJtVW5LUzVTWVc1clRtRnRaU2dwSUhSb1pXNEtJQ0FnSUNBZ0lDQnRjUzVqYldRb0p5OXpkRzl3Wkdsell5Y3BDaUFnSUNCbGJtUUtJQ0FnSUcxeExtUmxiR0Y1S0RFd0tRb2dJQ0FnZDJocGJHVWdiM011ZEdsdFpTZ3BJQzBnYzNSaGNuUlVhVzFsSUR3OUlERXhJR1J2Q2lBZ0lDQWdJQ0FnYlhFdVkyMWtLQ2N2Ym1GMklITndZWGR1SUVKbGRHaDFiaWNwQ2lBZ0lDQWdJQ0FnYVdZZ2JYRXVWRXhQTGxOd1lYZHVLQ2RDWlhSb2RXNG5LUzVFYVhOMFlXNWpaU2dwSUQ0Z01UQWdkR2hsYmdvZ0lDQWdJQ0FnSUNBZ0lDQnRjUzVqYldRb0p5OXVZWFlnYzNCaGQyNGdRbVYwYUhWdUp5a0tJQ0FnSUNBZ0lDQmxibVFLSUNBZ0lDQWdJQ0J0Y1M1a1pXeGhlU2d4TURBcENpQWdJQ0JsYm1RS1pXNWtDZ3B5WlhSMWNtNGdlMlYyWlc1MFpuVnVZejFsZG1WdWRGOW9ZVzVrYkdWeWZRPT0iLAogY2F0ZWdvcnkgPSAiVG9WIiwKIHR5cGUgPSAiZXZlbnRzIiwKIG5hbWUgPSAiR3Jpa2xvclNsZWV0IiwKIHBhdHRlcm4gPSAiIyojR2F0aGVyISBUaGlzIGZpcmUgc2hvdWxkIHByb3RlY3QgeW91IGZyb20gdGhlIHNsZWV0ISMqIyIsCn0=
+cmV0dXJuIHsKIGxvYWQgPSB7CiAgYWx3YXlzID0gZmFsc2UsCiAgY2xhc3MgPSAiIiwKICB6b25lID0gIiIsCiB9LAogY29kZSA9ICJiRzlqWVd3Z2JYRWdQU0J5WlhGMWFYSmxLQ2R0Y1NjcENteHZZMkZzSUc5eklEMGdjbVZ4ZFdseVpTZ25iM01uS1Fwc2IyTmhiQ0J0ZVY5amJHRnpjeUE5SUcxeExsUk1UeTVOWlM1RGJHRnpjeTVUYUc5eWRFNWhiV1VvS1FvS2JHOWpZV3dnWm5WdVkzUnBiMjRnWW5Ka1lXTjBhVzl1S0NrS0lDQWdJRzF4TG1OdFpDZ25MMkp2ZUhJZ1VHRjFjMlVuS1FvZ0lDQWdiWEV1WTIxa0tDY3ZZWFIwWVdOcklHOW1aaWNwQ2lBZ0lDQnRjUzVqYldRb0p5OXJaWGx3Y21WemN5QmxjMk1uS1FvZ0lDQWdiWEV1WkdWc1lYa29NVEFwQ2lBZ0lDQnRjUzVqYldRb0p5OTBkMmx6ZENCemRHOXdKeWtLSUNBZ0lHMXhMbU50WkNnbkwyNWhkaUJ6Y0dGM2JpQkNaWFJvZFc0bktRb2dJQ0FnYlhFdVpHVnNZWGtvTXpBcENpQWdJQ0J0Y1M1amJXUW9KeTlpYjNoeUlGVnVjR0YxYzJVbktRb2dJQ0FnYlhFdVkyMWtLQ2N2WW05NGNpQkRZVzF3SnlrS0lDQWdJRzF4TG1OdFpDZ25MM1IzYVhOMElITjBZWEowSnlrS1pXNWtDZ3BzYjJOaGJDQm1kVzVqZEdsdmJpQnJZV0ZqZEdsdmJpZ3BDaUFnSUNCdGNTNWpiV1FvSnk5aWIzaHlJRkJoZFhObEp5a0tJQ0FnSUcxeExtTnRaQ2duTDJGMGRHRmpheUJ2Wm1ZbktRb2dJQ0FnYlhFdVkyMWtLQ2N2YTJWNWNISmxjM01nWlhOakp5a0tJQ0FnSUcxeExtUmxiR0Y1S0RFd0tRb2dJQ0FnYlhFdVkyMWtLQ2N2Ym1GMklITndZWGR1SUVKbGRHaDFiaWNwQ2lBZ0lDQnRjUzVrWld4aGVTZ3pNQ2tLSUNBZ0lHMXhMbU50WkNnbkwySnZlSElnVlc1d1lYVnpaU2NwQ2lBZ0lDQnRjUzVqYldRb0p5OWliM2h5SUVOaGJYQW5LUXBsYm1RS0NteHZZMkZzSUdaMWJtTjBhVzl1SUhSaGJtdGhZM1JwYjI0b0tRb2dJQ0FnYkc5allXd2djM1JoY25SVWFXMWxJRDBnYjNNdWRHbHRaU2dwQ2lBZ0lDQnRjUzVqYldRb0p5OWpkM1J1Y3lCdGIyUmxJRGNuS1FvZ0lDQWdkMmhwYkdVZ2IzTXVkR2x0WlNncElDMGdjM1JoY25SVWFXMWxJRHc5SURNZ1pHOEtJQ0FnSUNBZ0lDQnRjUzVqYldRb0p5OXVZWFlnYzNCaGQyNGdRbVYwYUhWdUp5a0tJQ0FnSUNBZ0lDQnBaaUJ0Y1M1VVRFOHVVM0JoZDI0b0owSmxkR2gxYmljcExrUnBjM1JoYm1ObEtDa2dQaUF4TlNCMGFHVnVDaUFnSUNBZ0lDQWdJQ0FnSUcxeExtTnRaQ2duTDI1aGRpQnpjR0YzYmlCQ1pYUm9kVzRuS1FvZ0lDQWdJQ0FnSUdWdVpBb2dJQ0FnWlc1a0NpQWdJQ0J0Y1M1amJXUW9KeTlqZDNSdWN5QnRiMlJsSURjbktRb2dJQ0FnYlhFdVkyMWtLQ2N2WTNkMGJuTWdjbVZ6WlhSallXMXdKeWtLWlc1a0NncHNiMk5oYkNCbWRXNWpkR2x2YmlCamQzUnVZV04wYVc5dUtDa0tJQ0FnSUd4dlkyRnNJSE4wWVhKMFZHbHRaU0E5SUc5ekxuUnBiV1VvS1FvZ0lDQWdiWEV1WTIxa0tDY3ZZbTk0Y2lCUVlYVnpaU2NwQ2lBZ0lDQnRjUzVqYldRb0p5OWpkM1J1Y3lCdGIyUmxJREFuS1FvZ0lDQWdiWEV1WTIxa0tDY3ZZWFIwWVdOcklHOW1aaWNwQ2lBZ0lDQnRjUzVqYldRb0p5OXJaWGx3Y21WemN5QmxjMk1uS1FvZ0lDQWdkMmhwYkdVZ2IzTXVkR2x0WlNncElDMGdjM1JoY25SVWFXMWxJRHc5SURNZ1pHOEtJQ0FnSUNBZ0lDQnRjUzVqYldRb0p5OXVZWFlnYzNCaGQyNGdRbVYwYUhWdUp5a0tJQ0FnSUNBZ0lDQnBaaUJ0Y1M1VVRFOHVVM0JoZDI0b0owSmxkR2gxYmljcExrUnBjM1JoYm1ObEtDa2dQaUF4TlNCMGFHVnVDaUFnSUNBZ0lDQWdJQ0FnSUcxeExtTnRaQ2duTDI1aGRpQnpjR0YzYmlCQ1pYUm9kVzRuS1FvZ0lDQWdJQ0FnSUdWdVpBb2dJQ0FnWlc1a0NpQWdJQ0J0Y1M1a1pXeGhlU2d4TUNrS0lDQWdJRzF4TG1OdFpDZ25MMkp2ZUhJZ1ZXNXdZWFZ6WlNjcENpQWdJQ0J0Y1M1amJXUW9KeTlpYjNoeUlFTmhiWEFuS1FvZ0lDQWdiWEV1WTIxa0tDY3ZZM2QwYm5NZ2JXOWtaU0F4SnlrS1pXNWtDZ3BzYjJOaGJDQm1kVzVqZEdsdmJpQm9aV0ZzWVdOMGFXOXVLQ2tLSUNBZ0lHeHZZMkZzSUhOMFlYSjBWR2x0WlNBOUlHOXpMblJwYldVb0tRb2dJQ0FnYlhFdVkyMWtLQ2N2WTNkMGJuTWdiVzlrWlNBd0p5a0tJQ0FnSUcxeExtTnRaQ2duTDJGMGRHRmpheUJ2Wm1ZbktRb2dJQ0FnYlhFdVkyMWtLQ2N2YTJWNWNISmxjM01nWlhOakp5a0tJQ0FnSUhkb2FXeGxJRzl6TG5ScGJXVW9LU0F0SUhOMFlYSjBWR2x0WlNBOFBTQXpJR1J2Q2lBZ0lDQWdJQ0FnYlhFdVkyMWtLQ2N2Ym1GMklITndZWGR1SUVKbGRHaDFiaWNwQ2lBZ0lDQWdJQ0FnYVdZZ2JYRXVWRXhQTGxOd1lYZHVLQ2RDWlhSb2RXNG5LUzVFYVhOMFlXNWpaU2dwSUQ0Z01UVWdkR2hsYmdvZ0lDQWdJQ0FnSUNBZ0lDQnRjUzVqYldRb0p5OXVZWFlnYzNCaGQyNGdRbVYwYUhWdUp5a0tJQ0FnSUNBZ0lDQmxibVFLSUNBZ0lHVnVaQW9nSUNBZ2JYRXVaR1ZzWVhrb01UQXBDaUFnSUNCdGNTNWpiV1FvSnk5aWIzaHlJRU5oYlhBbktRb2dJQ0FnYlhFdVkyMWtLQ2N2WTNkMGJuTWdiVzlrWlNBeEp5a0taVzVrQ2dvS2JHOWpZV3dnWVdOMGFXOXVjeUE5SUhzS0lDQWdJRUpTUkNBOUlHSnlaR0ZqZEdsdmJpd0tJQ0FnSUZKT1J5QTlJR3RoWVdOMGFXOXVMQW9nSUNBZ1YwbGFJRDBnYTJGaFkzUnBiMjRzQ2lBZ0lDQlFRVXdnUFNCMFlXNXJZV04wYVc5dUxBb2dJQ0FnVTBoRUlEMGdkR0Z1YTJGamRHbHZiaXdLSUNBZ0lGZEJVaUE5SUhSaGJtdGhZM1JwYjI0c0NpQWdJQ0JDUlZJZ1BTQmpkM1J1WVdOMGFXOXVMQW9nSUNBZ1FsTlVJRDBnWTNkMGJtRmpkR2x2Yml3S0lDQWdJRVZPUXlBOUlHTjNkRzVoWTNScGIyNHNDaUFnSUNCTlFVY2dQU0JqZDNSdVlXTjBhVzl1TEFvZ0lDQWdUVTVMSUQwZ1kzZDBibUZqZEdsdmJpd0tJQ0FnSUU1RlF5QTlJR04zZEc1aFkzUnBiMjRzQ2lBZ0lDQlNUMGNnUFNCamQzUnVZV04wYVc5dUxBb2dJQ0FnUTB4U0lEMGdhR1ZoYkdGamRHbHZiaXdLSUNBZ0lGTklUU0E5SUdobFlXeGhZM1JwYjI0c0NpQWdJQ0JFVWxVZ1BTQnJZV0ZqZEdsdmJpd0tmUW9LYkc5allXd2dablZ1WTNScGIyNGdaWFpsYm5SZmFHRnVaR3hsY2lncENpQWdJQ0J5WlhSMWNtNGdZV04wYVc5dWMxdHRlVjlqYkdGemMxMG9LUXBsYm1RS0NuSmxkSFZ5YmlCN1pYWmxiblJtZFc1alBXVjJaVzUwWDJoaGJtUnNaWEo5IiwKIGNhdGVnb3J5ID0gIlRvViIsCiB0eXBlID0gImV2ZW50cyIsCiBuYW1lID0gIkdyaWtsb3JTbGVldCIsCiBwYXR0ZXJuID0gIiMqI0dhdGhlciEgVGhpcyBmaXJlIHNob3VsZCBwcm90ZWN0IHlvdSBmcm9tIHRoZSBzbGVldCEjKiMiLAp9
 
 Actual Code:
 
@@ -10,19 +10,99 @@ local mq = require('mq')
 local os = require('os')
 local my_class = mq.TLO.Me.Class.ShortName()
 
-local function event_handler()
-    local startTime = os.time()
-    if my_class == 'BER' and mq.TLO.Me.ActiveDisc.Name() == mq.TLO.Spell('Frenzied Resolve Discipline').RankName() then
-        mq.cmd('/stopdisc')
-    end
+local function brdaction()
+    mq.cmd('/boxr Pause')
+    mq.cmd('/attack off')
+    mq.cmd('/keypress esc')
     mq.delay(10)
-    while os.time() - startTime <= 11 do
+    mq.cmd('/twist stop')
+    mq.cmd('/nav spawn Bethun')
+    mq.delay(30)
+    mq.cmd('/boxr Unpause')
+    mq.cmd('/boxr Camp')
+    mq.cmd('/twist start')
+end
+
+local function kaaction()
+    mq.cmd('/boxr Pause')
+    mq.cmd('/attack off')
+    mq.cmd('/keypress esc')
+    mq.delay(10)
+    mq.cmd('/nav spawn Bethun')
+    mq.delay(30)
+    mq.cmd('/boxr Unpause')
+    mq.cmd('/boxr Camp')
+end
+
+local function tankaction()
+    local startTime = os.time()
+    mq.cmd('/cwtns mode 7')
+    while os.time() - startTime <= 3 do
         mq.cmd('/nav spawn Bethun')
-        if mq.TLO.Spawn('Bethun').Distance() > 10 then
+        if mq.TLO.Spawn('Bethun').Distance() > 15 then
             mq.cmd('/nav spawn Bethun')
         end
-        mq.delay(100)
     end
+    mq.cmd('/cwtns mode 7')
+    mq.cmd('/cwtns resetcamp')
+end
+
+local function cwtnaction()
+    local startTime = os.time()
+    mq.cmd('/boxr Pause')
+    mq.cmd('/cwtns mode 0')
+    mq.cmd('/attack off')
+    mq.cmd('/keypress esc')
+    while os.time() - startTime <= 3 do
+        mq.cmd('/nav spawn Bethun')
+        if mq.TLO.Spawn('Bethun').Distance() > 15 then
+            mq.cmd('/nav spawn Bethun')
+        end
+    end
+    mq.delay(10)
+    mq.cmd('/boxr Unpause')
+    mq.cmd('/boxr Camp')
+    mq.cmd('/cwtns mode 1')
+end
+
+local function healaction()
+    local startTime = os.time()
+    mq.cmd('/cwtns mode 0')
+    mq.cmd('/attack off')
+    mq.cmd('/keypress esc')
+    while os.time() - startTime <= 3 do
+        mq.cmd('/nav spawn Bethun')
+        if mq.TLO.Spawn('Bethun').Distance() > 15 then
+            mq.cmd('/nav spawn Bethun')
+        end
+    end
+    mq.delay(10)
+    mq.cmd('/boxr Camp')
+    mq.cmd('/cwtns mode 1')
+end
+
+
+local actions = {
+    BRD = brdaction,
+    RNG = kaaction,
+    WIZ = kaaction,
+    PAL = tankaction,
+    SHD = tankaction,
+    WAR = tankaction,
+    BER = cwtnaction,
+    BST = cwtnaction,
+    ENC = cwtnaction,
+    MAG = cwtnaction,
+    MNK = cwtnaction,
+    NEC = cwtnaction,
+    ROG = cwtnaction,
+    CLR = healaction,
+    SHM = healaction,
+    DRU = kaaction,
+}
+
+local function event_handler()
+    return actions[my_class]()
 end
 
 return {eventfunc=event_handler}
