6632dc2de49466c364403decb21d633b71160168
Shadow <96359879+ShadowRG2023@users.noreply.github.com>
2023-03-16 12:34:05 -0400

    Add files via upload

---
+++ GriklorAirMode.lua
@@ -0,0 +1,80 @@
+GriklorAirMode
+
+Import Code:
+
+cmV0dXJuIHsKIGxvYWQgPSB7CiAgYWx3YXlzID0gZmFsc2UsCiAgY2xhc3MgPSAiIiwKICB6b25lID0gIiIsCiB9LAogY29kZSA9ICJiRzlqWVd3Z2JYRWdQU0J5WlhGMWFYSmxLQ2R0Y1NjcENncHNiMk5oYkNCdGVWOWpiR0Z6Y3lBOUlHMXhMbFJNVHk1TlpTNURiR0Z6Y3k1VGFHOXlkRTVoYldVb0tRb0tiRzlqWVd3Z1puVnVZM1JwYjI0Z1luSmtZV04wYVc5dUtDa0tJQ0FnSUcxeExtTnRaQ2duTDJSbmRDQkpJR0Z0SUVKU1JDQlRkRzl3Y0dsdVp5QkJkSFJoWTJzaEp5a0tJQ0FnSUcxeExtTnRaQ2duTDJKdmVISWdVR0YxYzJVbktRb2dJQ0FnYlhFdVkyMWtLQ2N2ZEhkcGMzUWdjM1J2Y0NjcENpQWdJQ0J0Y1M1amJXUW9KeTloZEhSaFkyc2diMlptSnlrS0lDQWdJRzF4TG1OdFpDZ25MMnRsZVhCeVpYTnpJR1Z6WXljcENpQWdJQ0J0Y1M1a1pXeGhlU2d4TUNrS0lDQWdJRzF4TG1OdFpDZ25MMjVoZGlCemNHRjNiaUJ3WXlBOUp5NHViWEV1VkV4UExsSmhhV1F1VFdGcGJrRnpjMmx6ZEM1T1lXMWxLQ2twQ2lBZ0lDQnRjUzVqYldRb0p5OWliM2h5SUZKaGFXUkJjM05wYzNST2RXMGdNU2NwQ2lBZ0lDQnRjUzVqYldRb0p5OWliM2h5SUZWdWNHRjFjMlVuS1FvZ0lDQWdiWEV1WTIxa0tDY3ZZbTk0Y2lCRGFHRnpaU2NwQ2lBZ0lDQnRjUzVqYldRb0p5OTBkMmx6ZENCemRHRnlkQ2NwQ21WdVpBb0tiRzlqWVd3Z1puVnVZM1JwYjI0Z2EyRmhZM1JwYjI0b0tRb2dJQ0FnYlhFdVkyMWtLQ2N2WkdkMElFa2dZVzBnUzBFZ1ZHOXZiaUJUZEc5d2NHbHVaeUJCZEhSaFkyc2hKeWtLSUNBZ0lHMXhMbU50WkNnbkwySnZlSElnVUdGMWMyVW5LUW9nSUNBZ2JYRXVZMjFrS0NjdllYUjBZV05ySUc5bVppY3BDaUFnSUNCdGNTNWpiV1FvSnk5clpYbHdjbVZ6Y3lCbGMyTW5LUW9nSUNBZ2JYRXVaR1ZzWVhrb01UQXBDaUFnSUNCdGNTNWpiV1FvSnk5dVlYWWdjM0JoZDI0Z2NHTWdQU2N1TG0xeExsUk1UeTVTWVdsa0xrMWhhVzVCYzNOcGMzUXVUbUZ0WlNncEtRb2dJQ0FnYlhFdVkyMWtLQ2N2WW05NGNpQlZibkJoZFhObEp5a0tJQ0FnSUcxeExtTnRaQ2duTDJKdmVISWdVbUZwWkVGemMybHpkRTUxYlNBeEp5a0tJQ0FnSUcxeExtTnRaQ2duTDJKdmVISWdRMmhoYzJVbktRcGxibVFLQ214dlkyRnNJR1oxYm1OMGFXOXVJSFJoYm10aFkzUnBiMjRvS1FvZ0lDQWdiWEV1WTIxa0tDY3ZaR2QwSUVrZ1lXMGdRMnhsWVhKcGJtY2dSM0pwYXlCaGN5QlVZWEpuWlhRaEp5a0tJQ0FnSUcxeExtTnRaQ2duTDJOM2RHNXpJRzF2WkdVZ01DY3BDaUFnSUNCdGNTNWpiV1FvSnk5aGRIUmhZMnNnYjJabUp5a0tJQ0FnSUcxeExtTnRaQ2duTDJ0bGVYQnlaWE56SUdWell5Y3BDaUFnSUNCdGNTNWtaV3hoZVNneE1Da0tJQ0FnSUcxeExtTnRaQ2duTDJOM2RHNXpJRzF2WkdVZ055Y3BDaUFnSUNCdGNTNWpiV1FnS0NjdlkzZDBibk1nY21WelpYUmpZVzF3SnlrS1pXNWtDZ3BzYjJOaGJDQm1kVzVqZEdsdmJpQmpkM1J1WVdOMGFXOXVLQ2tLSUNBZ0lHMXhMbU50WkNnbkwyUm5kQ0JKSUdGdElFTlhWRTRnVTNSdmNIQnBibWNnUVhSMFlXTnJJU2NwQ2lBZ0lDQnRjUzVqYldRb0p5OWliM2h5SUZCaGRYTmxKeWtLSUNBZ0lHMXhMbU50WkNnbkwyTjNkRzV6SUcxdlpHVWdNQ2NwQ2lBZ0lDQnRjUzVqYldRb0p5OWhkSFJoWTJzZ2IyWm1KeWtLSUNBZ0lHMXhMbU50WkNnbkwydGxlWEJ5WlhOeklHVnpZeWNwQ2lBZ0lDQnRjUzVrWld4aGVTZ3hNQ2tLSUNBZ0lHMXhMbU50WkNnbkwySnZlSElnVlc1d1lYVnpaU2NwQ2lBZ0lDQnRjUzVqYldRb0p5OWpkM1J1Y3lCdGIyUmxJREluS1FwbGJtUUtDbXh2WTJGc0lHRmpkR2x2Ym5NZ1BTQjdDaUFnSUNCQ1VrUWdQU0JpY21SaFkzUnBiMjRzQ2lBZ0lDQlNUa2NnUFNCcllXRmpkR2x2Yml3S0lDQWdJRmRKV2lBOUlHdGhZV04wYVc5dUxBb2dJQ0FnVUVGTUlEMGdkR0Z1YTJGamRHbHZiaXdLSUNBZ0lGTklSQ0E5SUhSaGJtdGhZM1JwYjI0c0NpQWdJQ0JYUVZJZ1BTQjBZVzVyWVdOMGFXOXVMQW9nSUNBZ1FrVlNJRDBnWTNkMGJtRmpkR2x2Yml3S0lDQWdJRUpUVkNBOUlHTjNkRzVoWTNScGIyNHNDaUFnSUNCRlRrTWdQU0JqZDNSdVlXTjBhVzl1TEFvZ0lDQWdUVUZISUQwZ1kzZDBibUZqZEdsdmJpd0tJQ0FnSUUxT1N5QTlJR04zZEc1aFkzUnBiMjRzQ2lBZ0lDQk9SVU1nUFNCamQzUnVZV04wYVc5dUxBb2dJQ0FnVWs5SElEMGdZM2QwYm1GamRHbHZiaXdnSUNBZ0lDQWdDbjBLQ214dlkyRnNJR1oxYm1OMGFXOXVJR1YyWlc1MFgyaGhibVJzWlhJb0tRb2dJQ0FnY21WMGRYSnVJR0ZqZEdsdmJuTmJiWGxmWTJ4aGMzTmRLQ2tLWlc1a0NncHlaWFIxY200Z2UyVjJaVzUwWm5WdVl6MWxkbVZ1ZEY5b1lXNWtiR1Z5ZlE9PSIsCiBjYXRlZ29yeSA9ICJUb1YiLAogdHlwZSA9ICJldmVudHMiLAogbmFtZSA9ICJHcmlrbG9yQWlyTW9kZSIsCiBwYXR0ZXJuID0gIiMqI0dyaWtsb3IgbGV0cyBvdXQgYSB0dW5lbGVzcyB3aGlzdGxlIyojIiwKfQ==
+
+Actual Code:
+
+local mq = require('mq')
+
+local my_class = mq.TLO.Me.Class.ShortName()
+
+local function brdaction()
+    mq.cmd('/dgt I am BRD Stopping Attack!')
+    mq.cmd('/boxr Pause')
+    mq.cmd('/twist stop')
+    mq.cmd('/attack off')
+    mq.cmd('/keypress esc')
+    mq.delay(10)
+    mq.cmd('/nav spawn pc ='..mq.TLO.Raid.MainAssist.Name())
+    mq.cmd('/boxr RaidAssistNum 1')
+    mq.cmd('/boxr Unpause')
+    mq.cmd('/boxr Chase')
+    mq.cmd('/twist start')
+end
+
+local function kaaction()
+    mq.cmd('/dgt I am KA Toon Stopping Attack!')
+    mq.cmd('/boxr Pause')
+    mq.cmd('/attack off')
+    mq.cmd('/keypress esc')
+    mq.delay(10)
+    mq.cmd('/nav spawn pc ='..mq.TLO.Raid.MainAssist.Name())
+    mq.cmd('/boxr Unpause')
+    mq.cmd('/boxr RaidAssistNum 1')
+    mq.cmd('/boxr Chase')
+end
+
+local function tankaction()
+    mq.cmd('/dgt I am Clearing Grik as Target!')
+    mq.cmd('/cwtns mode 0')
+    mq.cmd('/attack off')
+    mq.cmd('/keypress esc')
+    mq.delay(10)
+    mq.cmd('/cwtns mode 7')
+    mq.cmd ('/cwtns resetcamp')
+end
+
+local function cwtnaction()
+    mq.cmd('/dgt I am CWTN Stopping Attack!')
+    mq.cmd('/boxr Pause')
+    mq.cmd('/cwtns mode 0')
+    mq.cmd('/attack off')
+    mq.cmd('/keypress esc')
+    mq.delay(10)
+    mq.cmd('/boxr Unpause')
+    mq.cmd('/cwtns mode 2')
+end
+
+local actions = {
+    BRD = brdaction,
+    RNG = kaaction,
+    WIZ = kaaction,
+    PAL = tankaction,
+    SHD = tankaction,
+    WAR = tankaction,
+    BER = cwtnaction,
+    BST = cwtnaction,
+    ENC = cwtnaction,
+    MAG = cwtnaction,
+    MNK = cwtnaction,
+    NEC = cwtnaction,
+    ROG = cwtnaction,       
+}
+
+local function event_handler()
+    return actions[my_class]()
+end
+
+return {eventfunc=event_handler}
